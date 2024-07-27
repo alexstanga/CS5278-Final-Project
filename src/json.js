1e91068d68ef1d342352cc95fc29a9ea80755cf7
Alexander Stanga <alexstanga99@gmail.com>
2024-07-26 19:24:10 -0600

    Initial commit of survey

---
+++ src/json.js
@@ -0,0 +1,371 @@
+export const json = {
+    "completeText": "Finish",
+    "pageNextText": "Continue",
+    "pagePrevText": "Previous",
+    "pages": [
+        {
+            "elements": [
+                {
+                    "type": "panel",
+                    "elements": [
+                        {
+                            "type": "html",
+                            "name": "hra_intro",
+                            "html": "<article class='intro'><div><h4 class='intro__heading'>Health Risk Assessment</h4><p class='intro__body'>Welcome to the Health Risk Assessment survey. This survey will ask you questions about your health, dietary habits, and exercise routines. Your responses will help us assess potential health risks and provide you with personalized health recommendations. Please answer each question as accurately as possible. All information you provide will be kept confidential and used solely for the purpose of this assessment.</p><br><p>Let's begin.</p></div></article>"
+                        }
+                    ],
+                    "name": "panel0"
+                },
+                {
+                    "type": "panel",
+                    "elements": [
+                        {
+                            "type": "text",
+                            "name": "firstname_c",
+                            "title": "First Name"
+                        }
+                    ],
+                    "name": "panel0-1"
+                }
+            ],
+            "name": "page0"
+        },
+        {
+            "elements": [
+                {
+                    "type": "panel",
+                    "elements": [
+                        {
+                            "type": "radiogroup",
+                            "choices": [
+                                "I do not eat it at all",
+                                "Less than 1 serving per week 1-2 servings per week",
+                                "3-4 servings per week",
+                                "5-6 servings per week",
+                                "1 serving per day",
+                                "2-3 servings per day",
+                                "4-5 servings per day",
+                                "6 or more servings per day",
+
+                            ],
+                            "name": "freshfruit_c",
+                            "title": " "
+                        }
+                    ],
+                    "name": "panel1",
+                    "title": "How often do you eat fresh fruit?"
+                }
+            ],
+            "name": "page1"
+        },
+        {
+            "elements": [
+                {
+                    "type": "panel",
+                    "elements": [
+                        {
+                            "type": "radiogroup",
+                            "choices": [
+                                "I do not eat it at all",
+                                "Less than 1 serving per week 1-2 servings per week",
+                                "3-4 servings per week",
+                                "5-6 servings per week",
+                                "1 serving per day",
+                                "2-3 servings per day",
+                                "4-5 servings per day",
+                                "6 or more servings per day",
+
+                            ],
+                            "name": "vegetables_c",
+                            "title": " "
+                        }
+                    ],
+                    "name": "panel2",
+                    "title": "How often do you eat vegetables?"
+                }
+
+            ],
+            "name": "page2"
+        },
+        {
+            "elements": [
+                {
+                    "type": "panel",
+                    "elements": [
+                        {
+                            "type": "radiogroup",
+                            "choices": [
+                                "I do not eat it at all",
+                                "Less than 1 serving per week 1-2 servings per week",
+                                "3-4 servings per week",
+                                "5-6 servings per week",
+                                "1 serving per day",
+                                "2-3 servings per day",
+                                "4-5 servings per day",
+                                "6 or more servings per day",
+
+                            ],
+                            "name": "legumes_c",
+                            "title": " "
+                        }
+                    ],
+                    "name": "panel3",
+                    "title": "How often do you eat legumes, nuts, seeds?"
+                }
+            ],
+            "name": "page3",
+        },
+        {
+            "elements": [
+                {
+                    "type": "panel",
+                    "elements": [
+                        {
+                            "type": "radiogroup",
+                            "choices": [
+                                "I do not eat it at all",
+                                "Less than 1 serving per week 1-2 servings per week",
+                                "3-4 servings per week",
+                                "5-6 servings per week",
+                                "1 serving per day",
+                                "2-3 servings per day",
+                                "4-5 servings per day",
+                                "6 or more servings per day",
+
+                            ],
+                            "name": "seafood_c",
+                            "title": " "
+                        }
+                    ],
+                    "name": "panel4",
+                    "title": "How often do you eat fish or seafood?"
+                }
+            ],
+            "name": "page4",
+        },
+        {
+            "elements": [
+                {
+                    "type": "panel",
+                    "elements": [
+                        {
+                            "type": "radiogroup",
+                            "choices": [
+                                "I do not eat it at all",
+                                "Less than 1 serving per week 1-2 servings per week",
+                                "3-4 servings per week",
+                                "5-6 servings per week",
+                                "1 serving per day",
+                                "2-3 servings per day",
+                                "4-5 servings per day",
+                                "6 or more servings per day",
+
+                            ],
+                            "name": "wholegrains_c",
+                            "title": " "
+                        }
+                    ],
+                    "name": "panel5",
+                    "title": "How often do you eat whole grains?"
+                }
+            ],
+            "name": "page5",
+        },
+        {
+            "elements": [
+                {
+                    "type": "panel",
+                    "elements": [
+                        {
+                            "type": "radiogroup",
+                            "choices": [
+                                "I do not eat it at all",
+                                "Less than 1 serving per week 1-2 servings per week",
+                                "3-4 servings per week",
+                                "5-6 servings per week",
+                                "1 serving per day",
+                                "2-3 servings per day",
+                                "4-5 servings per day",
+                                "6 or more servings per day",
+
+                            ],
+                            "name": "refinedgrains_c",
+                            "title": " "
+                        }
+                    ],
+                    "name": "panel6",
+                    "title": "How often do you eat refined grains?"
+                }
+            ],
+            "name": "page6",
+        },
+        {
+            "elements": [
+                {
+                    "type": "panel",
+                    "elements": [
+                        {
+                            "type": "radiogroup",
+                            "choices": [
+                                "I do not eat it at all",
+                                "Less than 1 serving per week 1-2 servings per week",
+                                "3-4 servings per week",
+                                "5-6 servings per week",
+                                "1 serving per day",
+                                "2-3 servings per day",
+                                "4-5 servings per day",
+                                "6 or more servings per day",
+
+                            ],
+                            "name": "lowfatdairy_c",
+                            "title": " "
+                        }
+                    ],
+                    "name": "panel7",
+                    "title": "How often do you eat low-fat dairy?"
+                }
+            ],
+            "name": "page7",
+        },
+        {
+            "elements": [
+                {
+                    "type": "panel",
+                    "elements": [
+                        {
+                            "type": "radiogroup",
+                            "choices": [
+                                "I do not eat it at all",
+                                "Less than 1 serving per week 1-2 servings per week",
+                                "3-4 servings per week",
+                                "5-6 servings per week",
+                                "1 serving per day",
+                                "2-3 servings per day",
+                                "4-5 servings per day",
+                                "6 or more servings per day",
+
+                            ],
+                            "name": "highfatdairy_c",
+                            "title": " "
+                        }
+                    ],
+                    "name": "panel8",
+                    "title": "How often do you eat high-fat dairy and saturated fats?"
+                }
+            ],
+            "name": "page8",
+        },
+        {
+            "elements": [
+                {
+                    "type": "panel",
+                    "elements": [
+                        {
+                            "type": "radiogroup",
+                            "choices": [
+                                "I do not eat it at all",
+                                "Less than 1 serving per week 1-2 servings per week",
+                                "3-4 servings per week",
+                                "5-6 servings per week",
+                                "1 serving per day",
+                                "2-3 servings per day",
+                                "4-5 servings per day",
+                                "6 or more servings per day",
+
+                            ],
+                            "name": "sweets_c",
+                            "title": " "
+                        }
+                    ],
+                    "name": "panel9",
+                    "title": "How often do you eat sweets and sweet foods?"
+                }
+            ],
+            "name": "page9",
+        },
+        {
+            "elements": [
+                {
+                    "type": "panel",
+                    "elements": [
+                        {
+                            "type": "radiogroup",
+                            "choices": [
+                                "0",
+                                "1",
+                                "2",
+                                "3",
+                                "4",
+                                "5",
+                                "6",
+                                "7",
+
+                            ],
+                            "name": "daysofexercise_c",
+                            "title": " "
+                        }
+                    ],
+                    "name": "panel10",
+                    "title": "How many days of moderate to strenuous exercise, like a brisk walk, did you do in past last 7 days?"
+                }
+            ],
+            "name": "page10",
+        },
+        {
+            "elements": [
+                {
+                    "type": "panel",
+                    "elements": [
+                        {
+                            "type": "text",
+                            "name": "minutesofexercise_c",
+                            "title": " ",
+                            "isRequired": true,
+                            "requiredErrorText": "Value cannot be empty",
+                            "validators": [
+                                { "type": "numeric", "text": "Value must be a number"}
+                            ]
+                        }
+                    ],
+                    "name": "panel11",
+                    "title": "On those days that you engage in moderate to strenuous exercise, how many minutes, on average, do you exercise at this level? ___"
+                }
+            ],
+            "name": "page11",
+        },
+        {
+            "elements": [
+                {
+                    "type": "panel",
+                    "elements": [
+                        {
+                            "type": "dropdown",
+                            "name": "changeexercise_c",
+                            "title": "Readiness to Change",
+                            "choices": [
+                                "0 (Not Ready)",
+                                "1",
+                                "2",
+                                "3",
+                                "4",
+                                "5 (Unsure)",
+                                "6",
+                                "7",
+                                "8",
+                                "9",
+                                "10 (Ready)"
+                            ],
+                        }
+                    ],
+                    "name": "panel12",
+                    "title": "On a scale from 1 to 10, how ready are you to make changes in your exercise? "
+                }
+            ],
+            "name": "page12",
+        },
+
+    ],
+    "requiredText": "",
+    "showQuestionNumbers": "off",
+    "storeOthersAsComment": false
+};
